870e3ecabcdc39e4bb2cdbb81be3d89dba7f1ec6
Dragos20Tech <dragospetrescu5@gmail.com>
2024-01-10 17:01:06 +0200

    DJ PostgresSetup & Migrate

--- btre_project/settings.py
+++ btre_project/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'btre_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btr_ed',
+        'USER': 'postgres',
+        'PASSWORD': 'student',
+        'HOST': 'localhost',
     }
 }
 
