eac53a07ce7869eb8b83489616d34fc759874a99
Dragos20Tech <dragospetrescu5@gmail.com>
2024-01-14 11:02:03 +0200

    Customized Admin Dispay Data

--- listings/admin.py
+++ listings/admin.py
@@ -5,4 +5,14 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')  # Listing details
+    list_display_links = ('id', 'title')  # Links to listing (id, title) in the admin area to make it clickable
+    list_filter = ('realtor',)  # Filter by realtor
+    list_editable = ('is_published',)  # Publish/ Unpublish
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+    list_per_page = 25  # Number of listings per page
+
+
+admin.site.register(Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -4,6 +4,15 @@ from django.contrib import admin
 
 from .models import Realtor
 
-admin.site.register(Realtor)  # registering a Django model called Realtor with the Django admin interface.
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
+
+admin.site.register(Realtor, RealtorAdmin)  # registering a Django model called Realtor with the Django admin interface.
 
 # After this registration, you can access and manage instances of the Realtor model through the Django admin panel.
+
