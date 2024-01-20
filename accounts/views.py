adfe6afbecb275a972e6870ec8909c2248f99f93
Dragos20Tech <dragospetrescu5@gmail.com>
2024-01-20 13:43:53 +0200

    Register & login templates

--- accounts/views.py
+++ accounts/views.py
@@ -4,11 +4,23 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        # Register User
+
+        return
+        # # Testing
+        # print('Submitted FORM', request.POST)
+        # return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')  # Login will submit to this
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        # Login User
+        return
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def dashboard(request):
